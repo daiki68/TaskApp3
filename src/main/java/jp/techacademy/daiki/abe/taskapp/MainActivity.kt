9f309f54b78c8715c556add758a56d0058f39875
h1229 <h12293812s@gmail.com>
2021-04-20 20:42:37 +0900

    課題「タスク管理アプリ機能追加」

---
+++ src/main/java/jp/techacademy/daiki/abe/taskapp/MainActivity.kt
@@ -0,0 +1,111 @@
+package jp.techacademy.daiki.abe.taskapp
+
+import android.app.AlarmManager
+import android.app.PendingIntent
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import io.realm.Realm
+import kotlinx.android.synthetic.main.activity_main.*
+import io.realm.RealmChangeListener
+import io.realm.Sort
+import android.content.Intent
+import androidx.appcompat.app.AlertDialog
+
+const val EXTRA_TASK = "jp.techacademy.daiki.abe.taskapp.TASK"
+
+class MainActivity : AppCompatActivity() {
+    private lateinit var mRealm: Realm
+    private val mRealmListener = object : RealmChangeListener<Realm> {
+        override fun onChange(element: Realm) {
+            reloadListView()
+        }
+    }
+
+    private lateinit var mTaskAdapter: TaskAdapter
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        button.setOnClickListener {
+            val a = mRealm.where(Task::class.java).equalTo("category",editText.text.toString()).findAll()
+
+            mTaskAdapter.mTaskList = mRealm.copyFromRealm(a)
+
+            listView1.adapter = mTaskAdapter
+
+            mTaskAdapter.notifyDataSetChanged()
+        }
+
+        fab.setOnClickListener { view ->
+            val intent = Intent(this, InputActivity::class.java)
+            startActivity(intent)
+        }
+
+        mRealm = Realm.getDefaultInstance()
+        mRealm.addChangeListener(mRealmListener)
+
+        mTaskAdapter = TaskAdapter(this)
+
+        listView1.setOnItemClickListener { parent, _, position, _ ->
+            val task = parent.adapter.getItem(position) as Task
+            val intent = Intent(this, InputActivity::class.java)
+            intent.putExtra(EXTRA_TASK, task.id)
+            startActivity(intent)
+        }
+
+        listView1.setOnItemLongClickListener { parent, _, position, _ ->
+            val task = parent.adapter.getItem(position) as Task
+
+            val builder = AlertDialog.Builder(this)
+
+            builder.setTitle("削除")
+            builder.setMessage(task.title + "削除しますか")
+
+            builder.setPositiveButton("OK"){_, _ ->
+                val results = mRealm.where(Task::class.java).equalTo("id", task.id).findAll()
+
+                mRealm.beginTransaction()
+                results.deleteAllFromRealm()
+                mRealm.commitTransaction()
+
+                val resultsIntent = Intent(applicationContext, TaskAlarmReceiver::class.java)
+                val resultPendingIntent = PendingIntent.getBroadcast(
+                    this,
+                    task.id,
+                    resultsIntent,
+                    PendingIntent.FLAG_UPDATE_CURRENT
+                )
+
+                val alarmManager = getSystemService(ALARM_SERVICE) as AlarmManager
+                alarmManager.cancel(resultPendingIntent)
+
+                reloadListView()
+            }
+
+            builder.setNegativeButton("CANCEL", null)
+
+            val dialog = builder.create()
+            dialog.show()
+
+            true
+        }
+
+        reloadListView()
+    }
+
+    private fun reloadListView() {
+
+        val taskRealmResults = mRealm.where(Task::class.java).findAll().sort("date", Sort.DESCENDING)
+
+        mTaskAdapter.mTaskList = mRealm.copyFromRealm(taskRealmResults)
+        listView1.adapter = mTaskAdapter
+        mTaskAdapter.notifyDataSetChanged()
+    }
+
+    override fun onDestroy() {
+        super.onDestroy()
+
+        mRealm.close()
+    }
+}
